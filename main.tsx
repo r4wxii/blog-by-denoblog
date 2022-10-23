e4e8daaabb441e30d2d99134373c3a4cb2461cc6
r4wxii <yn041010@gmail.com>
2022-10-23 17:58:32 +0900

    init

---
+++ main.tsx
@@ -0,0 +1,16 @@
+/** @jsx h */
+
+import blog, { h } from "blog";
+
+blog({
+  title: "No one knows unknowns",
+  avatar: "avatar.png",
+  author: "r4wxii",
+  links: [
+    { title: "Twitter", url: "https://twitter.com/r4wxii" },
+    { title: "GitHub", url: "https://github.com/r4wxii" },
+    { title: "Email", url: "mailto:contact@r4wxii.com" },
+  ],
+  lang: "jp",
+  favicon: "https://twemoji.maxcdn.com/v/latest/72x72/1f643.png",
+});
