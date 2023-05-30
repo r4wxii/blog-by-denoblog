5c93152d4fd11b99dfd0b783cc18316d38d56de2
r4wxii <contact@r4wxii.com>
2023-05-30 21:45:52 +0900

    はてなブックマークボタンを各記事下部に追加

--- main.tsx
+++ main.tsx
@@ -13,4 +13,30 @@ blog({
   ],
   lang: "jp",
   favicon: "https://twemoji.maxcdn.com/v/latest/72x72/1f643.png",
+  section: () => (
+    <section>
+      <a
+        href="https://b.hatena.ne.jp/entry/"
+        className="hatena-bookmark-button"
+        data-hatena-bookmark-layout="basic-label-counter"
+        data-hatena-bookmark-lang="ja"
+        title="このエントリーをはてなブックマークに追加"
+      >
+        <img
+          src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png"
+          alt="このエントリーをはてなブックマークに追加"
+          width="20"
+          height="20"
+          style="border: none;"
+        />
+      </a>
+      <script
+        type="text/javascript"
+        src="https://b.st-hatena.com/js/bookmark_button.js"
+        charSet="utf-8"
+        async="async"
+      >
+      </script>
+    </section>
+  ),
 });
