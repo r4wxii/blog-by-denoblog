ec05b2e5a868b05cf2b2d7e50210628c86d47f55
r4wxii <contact@r4wxii.com>
2023-06-14 23:04:50 +0900

    sectionをcomponentとして別ファイルへ

--- main.tsx
+++ main.tsx
@@ -2,6 +2,7 @@
 
 import blog, { h } from "blog";
 import defineConfig from "./uno.config.ts";
+import { Section } from "./section.tsx";
 
 blog({
   title: "No one knows unknowns",
@@ -14,33 +15,6 @@ blog({
   ],
   lang: "jp",
   favicon: "https://twemoji.maxcdn.com/v/latest/72x72/1f643.png",
-  section: () => (
-    <section
-      mt="8"
-    >
-      <a
-        href="https://b.hatena.ne.jp/entry/"
-        className="hatena-bookmark-button"
-        data-hatena-bookmark-layout="basic-label-counter"
-        data-hatena-bookmark-lang="ja"
-        title="このエントリーをはてなブックマークに追加"
-      >
-        <img
-          src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png"
-          alt="このエントリーをはてなブックマークに追加"
-          width="20"
-          height="20"
-          style="border: none;"
-        />
-      </a>
-      <script
-        type="text/javascript"
-        src="https://b.st-hatena.com/js/bookmark_button.js"
-        charSet="utf-8"
-        async="async"
-      >
-      </script>
-    </section>
-  ),
+  section: Section,
   unocss: defineConfig,
 });
